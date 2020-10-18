f233770951745dd3a6a81e32b3a3bc15bc8e7923
miladimos <message.tome1331@gmail.com>
2020-10-18 17:48:07 +0330

    Install logo component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//        config(['logging.channels.single.path' => \Phar::running()
+//            ? dirname(\Phar::running(false)) . '/desired-path/your-app.log'
+//            : storage_path('logs/your-app.log')
+//        ]);
     }
 
     /**
