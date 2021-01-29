ac19d91513a4580d8c481478e799d9360f54690b
miladimos <milad.jafari6210@gmail.com>
2021-01-29 22:52:59 +0330

    add PackageTemplateService

---
+++ app/Services/PackageTemplateService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class PackageTemplateService
+{
+    public function download($url)
+    {
+        //
+    }
+
+    public function extract($path)
+    {
+        //
+    }
+}
